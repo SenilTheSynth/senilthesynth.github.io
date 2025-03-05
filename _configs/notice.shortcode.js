fa13d5258ab7bc214fbcc0d7a546749f46b4db2e
SenilTheSynth <senil888+github@proton.me>
2025-03-04 23:53:01 -0800

    Oops.

--- _configs/notice.shortcode.js
+++
@@ -1,8 +0,0 @@
-export default function (data, noticeType, markdownLibrary) {
-  if (!noticeType) {
-    noticeType = "";
-  }
-  let noticeMarkup = markdownLibrary.renderInline(data);
-  return `<div class="notice ${noticeType}">${noticeMarkup}</div>`;
-}
-
